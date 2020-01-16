8e8883c6734719532b3dc6cb1736751016751331
Chandan Choubey <chandanwtb2009@gmail.com>
2020-01-16 11:09:12 +0530

    Spec coverage 100% across all files

--- spec/factories/game.rb
+++ spec/factories/game.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :game do
-
+    
     trait :with_frames do
       frames { build_list :frame, 3 }
     end
 
     trait :with_equal_to_10_frames do
-      frames { build_list :frame, 10 }
+      frames { build_list(:frame, 10) }
     end
 
     trait :with_frames_and_balls do
--- spec/models/frame_spec.rb
+++ spec/models/frame_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Frame, type: :model do
     expect(frame.game).to be_valid
   end
 
+  describe '#complete?' do
+    let(:frame){FactoryGirl.create(:frame)}
+
+    context 'when frames equal to 10' do
+      it "has a valid game record" do
+        allow_any_instance_of(Frame).to receive(:last?).and_return(true)
+        expect(frame.complete?).to be_falsey
+      end
+    end
+  end
+
 end
